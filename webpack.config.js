f4e258ae89bf8aa7cca313c42d3cab72241839d2
Ricard Figuls <ricard.figuls@gmail.com>
2019-03-14 22:48:14 +0100

    FIX #2

--- webpack.config.js
+++ webpack.config.js
@@ -8,7 +8,7 @@ module.exports = {
   output: {
     path: path.resolve(__dirname, "docs/"),
     filename: "simpl.js",
-    publicPath: ""
+    publicPath: "simpl/"
   },
   plugins: [
     new CopyWebpackPlugin([
