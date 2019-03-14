bbcb6506be79f2a72ee0b380916cf4d2e698a7a0
Ricard Figuls <ricard.figuls@gmail.com>
2019-03-14 22:44:30 +0100

    FIX deploy config

--- webpack.config.js
+++ webpack.config.js
@@ -8,7 +8,7 @@ module.exports = {
   output: {
     path: path.resolve(__dirname, "docs/"),
     filename: "simpl.js",
-    publicPath: "/simpl"
+    publicPath: ""
   },
   plugins: [
     new CopyWebpackPlugin([
