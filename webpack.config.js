4212a894c97c549a9ad259049903a8c6071f88fe
Ricard Figuls <ricard.figuls@gmail.com>
2019-03-14 22:41:46 +0100

    FIX publicPath

--- webpack.config.js
+++ webpack.config.js
@@ -8,7 +8,7 @@ module.exports = {
   output: {
     path: path.resolve(__dirname, "docs/"),
     filename: "simpl.js",
-    publicPath: "docs/"
+    publicPath: "/simpl"
   },
   plugins: [
     new CopyWebpackPlugin([
